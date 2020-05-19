c631f0a49851187a382f433867b00c71ed402f17
Nitish Bhalla <nitish.bhallaf5@gmail.com>
2020-05-19 09:06:26 +0530

    uploading the files

---
+++ vgg16/face_recog_using_vgg16.py
@@ -0,0 +1,223 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Making a Face recog with VGG16
+# 
+# ### Loading the VGG16 Model
+
+# In[1]:
+
+
+from keras.applications import VGG16
+
+# VGG16 was designed to work on 224 x 224 pixel input images sizes
+img_rows = 224
+img_cols = 224 
+
+#Loads the VGG16 model 
+model = VGG16(weights = 'imagenet', 
+                 include_top = False, 
+                 input_shape = (img_rows, img_cols, 3))
+
+
+# ### Inpsecting each layer
+
+# In[2]:
+
+
+# Let's print our layers 
+for (i,layer) in enumerate(model.layers):
+    print(str(i) + " "+ layer.__class__.__name__, layer.trainable)
+
+
+# ###  freeze all layers except the top 4 
+
+# In[3]:
+
+
+from keras.applications import VGG16
+
+# VGG16 was designed to work on 224 x 224 pixel input images sizes
+img_rows = 224
+img_cols = 224 
+
+# Re-loads the VGG16 model without the top or FC layers
+model = VGG16(weights = 'imagenet', 
+                 include_top = False, 
+                 input_shape = (img_rows, img_cols, 3))
+
+# Here we freeze the last 4 layers 
+# Layers are set to trainable as True by default
+for layer in model.layers:
+    layer.trainable = False
+    
+# Let's print our layers 
+for (i,layer) in enumerate(model.layers):
+    print(str(i) + " "+ layer.__class__.__name__, layer.trainable)
+
+
+# ### Let's make a function that returns our FC Head
+
+# In[4]:
+
+
+def addTopModel(bottom_model, num_classes, D=256):
+    """creates the top or head of the model that will be 
+    placed ontop of the bottom layers"""
+    top_model = bottom_model.output
+    top_model = Flatten(name = "flatten")(top_model)
+    top_model = Dense(D, activation = "relu")(top_model)
+    top_model = Dropout(0.3)(top_model)
+    top_model = Dense(num_classes, activation = "softmax")(top_model)
+    return top_model
+
+
+# In[5]:
+
+
+model.input
+
+
+# In[6]:
+
+
+model.layers
+
+
+# ### Let's add our FC Head back onto VGG
+
+# In[7]:
+
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D
+from keras.layers.normalization import BatchNormalization
+from keras.models import Model
+
+num_classes = 17
+
+FC_Head = addTopModel(model, num_classes)
+
+modelnew = Model(inputs=model.input, outputs=FC_Head)
+
+print(modelnew.summary())
+
+
+# In[ ]:
+
+
+
+
+
+# ### Loading our Face_recog  Dataset
+
+# In[8]:
+
+
+from keras.preprocessing.image import ImageDataGenerator
+
+train_data_dir = 'C://Users//Rajesh Bhalla//Desktop//facerecog1//training//'
+validation_data_dir = 'C://Users//Rajesh Bhalla//Desktop//facerecog1//valid//'
+
+train_datagen = ImageDataGenerator(
+      rescale=1./255,
+      rotation_range=20,
+      width_shift_range=0.2,
+      height_shift_range=0.2,
+      horizontal_flip=True,
+      fill_mode='nearest')
+ 
+validation_datagen = ImageDataGenerator(rescale=1./255)
+ 
+# Change the batchsize according to your system RAM
+train_batchsize = 16
+val_batchsize = 10
+ 
+train_generator = train_datagen.flow_from_directory(
+        train_data_dir,
+        target_size=(img_rows, img_cols),
+        batch_size=train_batchsize,
+        class_mode='categorical')
+ 
+validation_generator = validation_datagen.flow_from_directory(
+        validation_data_dir,
+        target_size=(img_rows, img_cols),
+        batch_size=val_batchsize,
+        class_mode='categorical',
+        shuffle=False)
+
+
+# ### Training our top layers
+
+# In[ ]:
+
+
+from keras.optimizers import RMSprop
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+                   
+checkpoint = ModelCheckpoint("face_recog2.h5",
+                             monitor="val_loss",
+                             mode="min",
+                             save_best_only = True,
+                             verbose=1)
+
+earlystop = EarlyStopping(monitor = 'val_loss', 
+                          min_delta = 0, 
+                          patience = 3,
+                          verbose = 1,
+                          restore_best_weights = True)
+
+# we put our call backs into a callback list
+callbacks = [earlystop, checkpoint]
+
+# Note we use a very small learning rate 
+modelnew.compile(loss = 'sparse_categorical_crossentropy',
+              optimizer = RMSprop(lr = 0.001),
+              metrics = ['accuracy'])
+
+nb_train_samples = 1190
+nb_validation_samples = 170
+epochs = 3
+batch_size = 64
+
+history = modelnew.fit_generator(
+    train_generator,
+    steps_per_epoch = nb_train_samples // batch_size,
+    epochs = epochs,
+    callbacks = callbacks,
+    validation_data = validation_generator,
+    validation_steps = nb_validation_samples // batch_size)
+
+modelnew.save("face_recog.h5")
+
+
+# 
+
+# In[ ]:
+
+
+
+
+
+# 
+
+# In[ ]:
+
+
+
+
+
+# 
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
---
+++ vgg16/training and loading the dataset.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#### loading the face_recog dataset
+
+from keras.preprocessing.image import ImageDataGenerator
+
+train_data_dir = 'C://Users//Rajesh Bhalla//Desktop//facerecog1//training//'
+validation_data_dir = 'C://Users//Rajesh Bhalla//Desktop//facerecog1//valid//'
+
+train_datagen = ImageDataGenerator(
+      rescale=1./255,
+      rotation_range=20,
+      width_shift_range=0.2,
+      height_shift_range=0.2,
+      horizontal_flip=True,
+      fill_mode='nearest')
+ 
+validation_datagen = ImageDataGenerator(rescale=1./255)
+ 
+# Change the batchsize according to your system RAM
+train_batchsize = 16
+val_batchsize = 10
+ 
+train_generator = train_datagen.flow_from_directory(
+        train_data_dir,
+        target_size=(img_rows, img_cols),
+        batch_size=train_batchsize,
+        class_mode='categorical')
+ 
+validation_generator = validation_datagen.flow_from_directory(
+        validation_data_dir,
+        target_size=(img_rows, img_cols),
+        batch_size=val_batchsize,
+        class_mode='categorical',
+        shuffle=False)
+
+
+# In[ ]:
+
+
+############## training the top layers 
+from keras.optimizers import RMSprop
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+                   
+checkpoint = ModelCheckpoint("face_recog2.h5",
+                             monitor="val_loss",
+                             mode="min",
+                             save_best_only = True,
+                             verbose=1)
+
+earlystop = EarlyStopping(monitor = 'val_loss', 
+                          min_delta = 0, 
+                          patience = 3,
+                          verbose = 1,
+                          restore_best_weights = True)
+
+# we put our call backs into a callback list
+callbacks = [earlystop, checkpoint]
+
+# Note we use a very small learning rate 
+modelnew.compile(loss = 'sparse_categorical_crossentropy',
+              optimizer = RMSprop(lr = 0.001),
+              metrics = ['accuracy'])
+
+nb_train_samples = 1190
+nb_validation_samples = 170
+epochs = 3
+batch_size = 64
+
+history = modelnew.fit_generator(
+    train_generator,
+    steps_per_epoch = nb_train_samples // batch_size,
+    epochs = epochs,
+    callbacks = callbacks,
+    validation_data = validation_generator,
+    validation_steps = nb_validation_samples // batch_size)
+
+modelnew.save("face_recog.h5")
+
